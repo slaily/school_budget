0270ce4ba0522acd41672d0684800e1365a1928a
slaily <slavov.iliyan96.com>
2022-02-10 15:36:45 +0200

    Add ML Model functionalities

--- school_budget/models.py
+++ school_budget/models.py
@@ -1,3 +1,6 @@
+from json import dumps
+from collections import OrderedDict
+
 from joblib import load
 from numpy import argsort
 
@@ -14,5 +17,18 @@ def top_n_predictions_ids(predicted, number=3):
     return argsort(predicted, axis=1)[:, -number:]
 
 
-def format_predictions():
-    pass
+def format_predictions(input_data, sorted_predictions, raw_predictions, labels, labels_ids):
+    return [
+        OrderedDict(
+            {
+                "label": labels[labels_ids[predicted_label]],
+                "probability": f"{raw_predictions[row][predicted_label]:.0%}"
+            }
+        )
+        for row, _ in enumerate(input_data)
+        for predicted_label in sorted_predictions[row]
+    ]
+
+
+def serialize_predictions(predictions):
+    return dumps(predictions)
